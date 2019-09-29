a95cbb7c1091e1ff843501c596194472ad617fc8
simas-code <55851746+simas-code@users.noreply.github.com>
2019-09-29 15:31:42 +0300

    update

--- src/UserInterface.java
+++ src/UserInterface.java
@@ -16,34 +16,45 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.math.BigDecimal;
 
-public class EMonitor {
-	public static void main(String[] args) {
+public class UserInterface {
+	private static final String INTRO = "Emonitor is a program which can be used for monitoring currency exchange rates data from Lbank.lt website.\n";
+	private static final String PARAM_NUMBER_ERROR = "Wrong number of parameters, for help write parameter -h.\nInitiating manual input.\n";
+	private static final String HELP = "Help.\n"
+			+ "Sending parameters through console is in this format: java Emonitor <ccy_list> <date_from> <date_to> .\n"
+			+ "Example_1: program AUD;USD;GBP 2019-09-01 2019-09-05.\n"
+			+ "Example_2: program USD 2019-09-04 2019-09-04.\n"
+			+ "---------------------------------------------\n"
+			+ "Input can be written manually using the same format.\n"
+			+ "Currency code should be 3 leters long, codes must be seperated by semicolons.\n"
+			+ "<ccy_list>: AUD;USD;GBP;.. \n"
+			+ "<date_from>: yyyy-mm-dd \n"
+			+ "<date_to>: yyyy-mm-dd \n";
+	
+	private static RequestManager rManager;
+	
+	public static void printCurrCodeList() {
+		System.out.print( "Available currency codes list:\n");
+		ArrayList<Request> templist =  new ArrayList<>();
 		try {
-			RequestManager rManager = new RequestManager();
-			
-			System.out.print("Emonitor is a program which can be used for monitoring currency exchange rates data from Lbank.lt website.\n");
+			templist.add(new Request(null,null,null,2));
+			rManager.setRequestList(templist);
+			rManager.sendRequests();
+		}catch (InvalidParameterException e) {
+			System.out.print("Wrong parameter values.");
+		}
+	}
+	
+	public static void main(String[] args) {
+		System.out.print(INTRO);
+		try {
+			rManager = new RequestManager();
 			if (args.length > 0) {
 				if (args[0].equals("-l")) {
-					System.out.print( "Available currency codes list:\n");
-					ArrayList<Request> templist =  new ArrayList<>();
-					templist.add(new Request(null,null,null,2));
-					rManager.setRequestList(templist);
-					rManager.sendRequests();
-					
+					printCurrCodeList();
 				} else if (args[0].equals("-h")) {
-					System.out.print( "Help.\n"
-					+ "Sending parameters through console is in this format: java Emonitor <ccy_list> <date_from> <date_to> .\n"
-					+ "Example_1: program AUD;USD;GBP 2019-09-01 2019-09-05.\n"
-					+ "Example_2: program USD 2019-09-04 2019-09-04.\n"
-					+ "---------------------------------------------\n"
-					+ "Input can be written manually using the same format.\n"
-					+ "Currency code should be 3 leters long, codes must be seperated by semicolons.\n"
-					+ "<ccy_list>: AUD;USD;GBP;.. \n"
-					+ "<date_from>: yyyy-mm-dd \n"
-					+ "<date_to>: yyyy-mm-dd \n");
+					System.out.print(HELP);
 				} else if (args.length != 3) {
-					System.out.print("Wrong number of parameters, for help write parameter -h.\n"
-					+ "Initiating manual input.\n");
+					System.out.print(PARAM_NUMBER_ERROR);
 					rManager.generateRequests(null);
 					rManager.sendRequests();
 				} else {
@@ -51,8 +62,7 @@ public class EMonitor {
 					rManager.sendRequests();
 				}
 			} else {
-				System.out.print("No parameters found, for help write parameter -h.\n"
-						+ "Initiating manual input.\n");
+				System.out.print(PARAM_NUMBER_ERROR);
 				rManager.generateRequests(null);
 				rManager.sendRequests();
 			}
